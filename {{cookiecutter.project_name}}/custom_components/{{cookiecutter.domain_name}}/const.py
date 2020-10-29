03859177e1be39d69ecff22d724a9e0f6f1487b4
oncleben31 <oncleben31@users.noreply.github.com>
2020-10-29 23:06:40 +0100

    First commit

---
+++ {{cookiecutter.project_name}}/custom_components/{{cookiecutter.domain_name}}/const.py
@@ -0,0 +1,40 @@
+"""Constants for blueprint."""
+# Base component constants
+NAME = "{{ cookiecutter.friendly_name }}"
+DOMAIN = "{{ cookiecutter.domain_name }}"
+DOMAIN_DATA = f"{DOMAIN}_data"
+VERSION = "{{ cookiecutter.version }}"
+
+ISSUE_URL = "https://github.com/{{cookiecutter.github_user}}/{{cookiecutter.project_name}}/issues"
+
+# Icons
+ICON = "mdi:format-quote-close"
+
+# Device classes
+BINARY_SENSOR_DEVICE_CLASS = "connectivity"
+
+# Platforms
+BINARY_SENSOR = "binary_sensor"
+SENSOR = "sensor"
+SWITCH = "switch"
+PLATFORMS = [BINARY_SENSOR, SENSOR, SWITCH]
+
+
+# Configuration and options
+CONF_ENABLED = "enabled"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+# Defaults
+DEFAULT_NAME = DOMAIN
+
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+{NAME}
+Version: {VERSION}
+This is a custom integration!
+If you have any issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
