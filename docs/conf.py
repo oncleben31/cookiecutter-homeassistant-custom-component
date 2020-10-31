7eae150065c8a3ee3f22f55d78457a9bf4f8665a
oncleben31 <oncleben31@users.noreply.github.com>
2020-10-31 09:26:57 +0100

    Add sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+
+project = "Home Assistant Custom Component Cookiecutter"
+author = "Oncleben31"
+copyright = f"{datetime.now().year}, {author}"
+# extensions = ["sphinx.ext.intersphinx"]
+# intersphinx_mapping = {"mypy": ("https://mypy.readthedocs.io/en/stable/", None)}
+language = "en"
+# html_static_path = ["_static"]
+html_theme = "alabaster"
+html_theme_options = {
+    "github_banner": "true",
+    "github_button": "true",
+    "github_count": "true",
+    "github_user": "oncleben31",
+    "github_repo": "cookiecutter-homeassistant-custom-component",
+    "github_type": "star",
+    # "logo": "logo.png",
+    "logo_name": "true",
+    "fixed_sidebar": "true",
+    "sidebar_width": "250px",
+}
---
+++ docs/quickstart.rst
@@ -0,0 +1,10 @@
+Quickstart Guide
+================
+
+.. include:: ../README.rst
+   :start-after: quickstart-begin
+   :end-before: quickstart-end
+
+.. include:: ../README.rst
+   :start-after: references-begin
+   :end-before: references-end
