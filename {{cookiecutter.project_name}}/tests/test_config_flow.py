8cd7413afa59dae09c4d10883482be5160faf432
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2023-04-24 04:15:00 -0300

    fix: set black fmt off in lines

--- {{cookiecutter.project_name}}/tests/test_config_flow.py
+++ {{cookiecutter.project_name}}/tests/test_config_flow.py
@@ -31,9 +31,9 @@ from .const import MOCK_CONFIG
 @pytest.fixture(autouse=True)
 def bypass_setup_fixture():
     """Prevent setup."""
-    with patch("custom_components.{{cookiecutter.domain_name}}.async_setup", return_value=True,), patch(
-        "custom_components.{{cookiecutter.domain_name}}.async_setup_entry",
-        return_value=True,
+    with patch("custom_components.{{cookiecutter.domain_name}}.async_setup", return_value=True,), patch( # fmt: off
+        "custom_components.{{cookiecutter.domain_name}}.async_setup_entry", # fmt: off
+        return_value=True, # fmt: off
     ):
         yield
 
