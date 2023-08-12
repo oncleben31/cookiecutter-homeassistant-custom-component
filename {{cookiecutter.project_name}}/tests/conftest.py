bd7b1854d6526bd0ab8ac59f222d2ad505e5858b
wolffshots <16850875+wolffshots@users.noreply.github.com>
2023-08-12 14:18:26 +0200

    chore: remove type annotations to conform

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 @pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations: FixtureRequest) -> None:
+def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations defined in the test dir."""
     yield
 
